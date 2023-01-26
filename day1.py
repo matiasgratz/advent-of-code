09336241c392858bbb1ad346fee74b54f13083bd
matiasgratz <mgratz@frba.utn.edu.ar>
2023-01-26 11:00:06 -0300

    Lista De Listas, Falta convertir Strings y sumar

--- day1.py
+++ day1.py
@@ -1,56 +1,23 @@
-"""
-~Paso 1 Leer el Archivo~
-**PENDIENTE** Paso 2 Guardar cada linea en un string
-**PENDIENTE** Paso 3 Detectar string iterando y guardar todos los items anteriores en una sub-lista
+# IT LIVES
+from itertools import groupby
+
+with open("input.txt") as f:
+    #Primero tengo que convertir los strings a int antes de que se metan en lista de listas porque si no, no se puede luego.
+    data = f.read().splitlines()
+    enNumeros = [ int(x) for x in data.split() ] 
+# result = [list(v) for k,v in groupby(data, key=str.isdigit)] - Convierte a lista de listas.
+
+print(enNumeros)
 
 """
+# Python3 program to Column wise sum of nested list
+ 
+def column_sum(lst):
+     
+    return list(map(sum, zip(*lst)))
+     
+# Driver code
+lst = [[1, 5, 3], [2, 7, 8], [4, 6, 9]]
+print(column_sum(lst))
 
-"""" SOLUCION PAUSADA
-
-# from itertools import groupby
-
-listaDeNumeros = []
-listaAgrupada = []
-# w = "\n"
-
-
-with open('input.txt') as file:
-    for line in file:
-
-        # listaDeNumeros.append(line.splitlines()) 
-        
-### listaAgrupada = [list(group) for key, group in groupby(listaDeNumeros, lambda x: x == w) if not key]   
-print(listaDeNumeros)
 """
-
-
-
-### V1: Perfecto funciona, tener una lista con todos los elementos y newline al final de cada conjunto.
-## ME quedo aca
-### This is trivial to do using list append through iteration (in a for loop) OR List comprehension
-
-
-listaDeNumeros = []
-listaAgrupada = []
-
-with open('input.txt') as f:
-    listaDeNumeros = f.read().splitlines()
-  
-print(listaDeNumeros)
-
-
-
-
-### V2: esto convierte a todo el texto en un string gigantezco separado por comas. donde el newline sigue ahi 
-"""from itertools import groupby
-
-listaDeNumeros = []
-listaAgrupada = []
-
-with open('input.txt') as file:
-    #listaDeNumeros = file.read().split()
-    listaDeNumeros = file.read().replace('\n', ',') ## esto convierte a todo el texto en un string gigantezco separado por comas. donde el newline sigue ahi    
-    [[str(x) for x in ss.split(',')] for ss in listaDeNumeros.split('\n')]
-print(listaDeNumeros)"""
-
-
