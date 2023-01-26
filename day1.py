5e02a7032909509f616fb5f2f3ea526576778868
matiasgratz <mgratz@frba.utn.edu.ar>
2023-01-25 23:18:06 -0300

    Commit Final del dia Varias Posibles soluciones

--- day1.py
+++ day1.py
@@ -5,6 +5,8 @@
 
 """
 
+"""" SOLUCION PAUSADA
+
 # from itertools import groupby
 
 listaDeNumeros = []
@@ -19,3 +21,36 @@ with open('input.txt') as file:
         
 ### listaAgrupada = [list(group) for key, group in groupby(listaDeNumeros, lambda x: x == w) if not key]   
 print(listaDeNumeros)
+"""
+
+
+
+### V1: Perfecto funciona, tener una lista con todos los elementos y newline al final de cada conjunto.
+## ME quedo aca
+### This is trivial to do using list append through iteration (in a for loop) OR List comprehension
+
+
+listaDeNumeros = []
+listaAgrupada = []
+
+with open('input.txt') as f:
+    listaDeNumeros = f.read().splitlines()
+  
+print(listaDeNumeros)
+
+
+
+
+### V2: esto convierte a todo el texto en un string gigantezco separado por comas. donde el newline sigue ahi 
+"""from itertools import groupby
+
+listaDeNumeros = []
+listaAgrupada = []
+
+with open('input.txt') as file:
+    #listaDeNumeros = file.read().split()
+    listaDeNumeros = file.read().replace('\n', ',') ## esto convierte a todo el texto en un string gigantezco separado por comas. donde el newline sigue ahi    
+    [[str(x) for x in ss.split(',')] for ss in listaDeNumeros.split('\n')]
+print(listaDeNumeros)"""
+
+
