84be513657ea7f4b6266b157e8f2ce0ee14f7068
matiasgratz <mgratz@frba.utn.edu.ar>
2023-01-25 22:30:26 -0300

    Commit Agrupacion

--- day1.py
+++ day1.py
@@ -4,7 +4,18 @@
 **PENDIENTE** Paso 3 Detectar string iterando y guardar todos los items anteriores en una sub-lista
 
 """
+
+# from itertools import groupby
+
+listaDeNumeros = []
+listaAgrupada = []
+# w = "\n"
+
+
 with open('input.txt') as file:
     for line in file:
-        print(line.splitlines())
+
+        # listaDeNumeros.append(line.splitlines()) 
         
+### listaAgrupada = [list(group) for key, group in groupby(listaDeNumeros, lambda x: x == w) if not key]   
+print(listaDeNumeros)
