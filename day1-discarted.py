320d9713fd2abe4c817f8bdbf19b85540af8a113
matiasgratz <mgratz@frba.utn.edu.ar>
2023-01-30 19:27:31 -0300

    Finished Day 1 Part a w/Matiasrossi

--- day1-discarted.py
+++ day1-discarted.py
@@ -6,11 +6,14 @@ with open("input.txt") as f:
     data = f.read().splitlines()
     result = [list(v) for k,v in groupby(data, key=str.isdigit)] #-Convierte a lista de listas.
     # Busco filtrar de la nested list las listas vacias
-    # listaSinVacias = list(filter(lambda x: x != '\n', result))
+        #listaSinVacias = list(filter(lambda x: (x == ' ') , result))
+        #listaSinVacias2 = [x for x in result if x != '\n']
+        #listaSinVacias3 = [x for x in result if x]
+            # Nada funciona, será por itertools?
     # Para luego poder usar la funcion de abajo que convierte los strings a enteros
     #nuevaLista = [int(g) for g in data]
 
-print(listaSinVacias)
+print(result)
 
 """ # Para la suma
 # Python3 program to Column wise sum of nested list
---
+++ day1b.py
@@ -0,0 +1,14 @@
+with open("input.txt", "r") as file:
+    sum = 0
+    current_elf_calories = 0
+    elf_max_calories = [0,0,0]
+    for line in file: # Leer cada linea
+
+        if line != "\n":    
+            current_elf_calories += int(line)
+        else:
+            if current_elf_calories > elf_max_calories:
+                elf_max_calories = current_elf_calories
+            current_elf_calories = 0
+
+    print(elf_max_calories)
