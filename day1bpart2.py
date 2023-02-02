9bfa4b72200f0c7324e8d4e79b94f5182d1439c6
matiasgratz <mgratz@frba.utn.edu.ar>
2023-02-01 22:04:35 -0300

    day1ab completed 🌠

--- day1bpart2.py
+++ day1bpart2.py
@@ -1,15 +1,15 @@
 with open("input.txt", "r") as file:
     sum = 0
     current_elf_calories = 0
-    elf_max_calories = 0
+    elf_max_calories = [0,0,0]
     for line in file: # Leer cada linea
 
         if line != "\n":    
             current_elf_calories += int(line)
         else:
-            if current_elf_calories > elf_max_calories:
-                elf_max_calories = current_elf_calories
+            if current_elf_calories >= elf_max_calories[-1]:
+                elf_max_calories.insert(0,current_elf_calories)
             current_elf_calories = 0
-
+    elf_max_calories.sort(reverse=True)
     print(elf_max_calories)
-
+    print(elf_max_calories[0]+elf_max_calories[1]+elf_max_calories[2])
